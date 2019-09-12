d12d970103aafeb9de167ad0499fb9d537c2e96d
ss3414 <3414523068@qq.com>
2019-09-12 13:54:37 +0800

    190912

--- springboot_es/src/main/java/com/common/config/WebMvcConfig.java
+++ springboot_es/src/main/java/com/common/config/WebMvcConfig.java
@@ -4,14 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     /* 中文乱码（fixme 会导致JSON转换失败） */
     @Override
