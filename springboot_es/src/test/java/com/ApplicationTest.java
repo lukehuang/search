7701c0fb679f27e4bdeeaf21bd159054d7ae2fd5
ss3414 <3414523068@qq.com>
2019-11-15 18:15:49 +0800

    191115

--- springboot_es/src/test/java/com/ApplicationTest.java
+++ springboot_es/src/test/java/com/ApplicationTest.java
@@ -1,18 +1,13 @@
 package com;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.module.demo.mapper.ProductMapper;
-import com.module.demo.model.Product;
 import com.module.demo.repository.ProductRepository;
-import org.elasticsearch.action.get.GetResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
